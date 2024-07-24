6071da99fcc9178131b37ab807d35b0ecd054d32
okada9200 <138983935+okada9200@users.noreply.github.com>
2024-07-24 16:50:27 +0900

    Delete route-planner directory

--- route-planner/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\RouteController;
-
-Route::get('/', [RouteController::class, 'index']);
-Route::post('/addresses', [RouteController::class, 'store']);
-Route::post('/get-route', [RouteController::class, 'getRoute']);
