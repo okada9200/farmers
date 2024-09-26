a0df7bfa76e90b2aa972b926c84f5b208ec3aaf6
Yuki <okadadesu9200@gmail.com>
2024-09-26 16:20:04 +0900

    一覧画面レスポンシブの対応

--- for-farmers/resources/views/crops/index.blade.php
+++ for-farmers/resources/views/crops/index.blade.php
@@ -17,41 +17,42 @@
             作物リスト
         </div>
         <div class="card-body">
-            <table class="table table-hover table-custom">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>名前</th>
-                        <th>種類</th>
-                        <th>品種</th>
-                        <th>植え付け日</th>
-                        <th>住所</th>
-                        <th>操作</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($crops as $crop)
+            <div class="table-responsive"> 
+                <table class="table table-hover table-custom">
+                    <thead>
                         <tr>
-                            <td>{{ $crop->id }}</td>
-                            <td>{{ $crop->name }}</td>
-                            <td>{{ $crop->type }}</td>
-                            <td>{{ $crop->variety }}</td>
-                            <td>{{ $crop->planting_date }}</td>
-                            <td>{{ $crop->address }}</td>
-                            <td>
-                                <a href="{{ route('crops.show', $crop->id) }}" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> 表示</a>
-                                <a href="{{ route('crops.edit', $crop->id) }}" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i> 編集</a>
-                                <!-- 削除ボタンに確認ダイアログを追加 -->
-                                <form action="{{ route('crops.destroy', $crop->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('本当に削除しますか？');">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i> 削除</button>
-                                </form>
-                            </td>
+                            <th>ID</th>
+                            <th>名前</th>
+                            <th class="d-none d-md-table-cell">種類</th> <!-- 中型以上の画面で表示 -->
+                            <th class="d-none d-md-table-cell">品種</th>
+                            <th class="d-none d-lg-table-cell">植え付け日</th> <!-- 大型以上の画面で表示 -->
+                            <th class="d-none d-lg-table-cell">住所</th>
+                            <th>操作</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($crops as $crop)
+                            <tr>
+                                <td>{{ $crop->id }}</td>
+                                <td>{{ $crop->name }}</td>
+                                <td class="d-none d-md-table-cell">{{ $crop->type }}</td>
+                                <td class="d-none d-md-table-cell">{{ $crop->variety }}</td>
+                                <td class="d-none d-lg-table-cell">{{ $crop->planting_date }}</td>
+                                <td class="d-none d-lg-table-cell">{{ $crop->address }}</td>
+                                <td>
+                                    <a href="{{ route('crops.show', $crop->id) }}" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> 表示</a>
+                                    <a href="{{ route('crops.edit', $crop->id) }}" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i> 編集</a>
+                                    <form action="{{ route('crops.destroy', $crop->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('本当に削除しますか？');">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i> 削除</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div> 
         </div>
     </div>
     @else
