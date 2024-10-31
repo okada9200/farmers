edfed125668e7c96d1dd84dabeb6ed0047c28615
Yuki <okadadesu9200@gmail.com>
2024-11-01 07:17:25 +0900

    追加

--- for-farmers/app/Providers/AppServiceProvider.php
+++ for-farmers/app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\App; // 追加
+use Illuminate\Support\Facades\URL; // 追加
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment('production','staging')) {
+            URL::forceScheme('https');
+        }
     }
 }
